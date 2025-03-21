cd2c0c9748a75c5eefd62a1b1a3ac5ae6894b334
Sachin Dadhaniya <sachindadhaniya@gmail.com>
2025-03-21 08:58:57 +0530

    Update index.ts

--- src/core/bot/index.ts
+++ src/core/bot/index.ts
@@ -1,15 +1,22 @@
-import { Bot } from "grammy";
+import { Bot, Context, session } from "grammy";
+import { type Conversation, type ConversationFlavor, conversations } from "@grammyjs/conversations";
+
+type MyContext = Context & ConversationFlavor;
+type MyConversation = Conversation<MyContext>;
 
 const bot = new Bot(String(process.env.BOT_TOKEN));
 
+// bot.use(session({ initial: () => ({}) }));
+// bot.use(conversations());
+
 bot.init()
-    .then((ctx) => {
-        console.log(`Berhasil masuk sebagai ${bot.botInfo.username} - ${bot.botInfo.id}`);
+    .then((client) => {
+        console.log(`Successfully logged in as ${bot.botInfo.username} - ${bot.botInfo.id}`);
     })
     .catch((err) => console.error(err));
 
 bot.api.setMyCommands([
-    { command: "start", description: "Mulai bot ini" },
+    { command: "start", description: "Start this bot" },
     { command: "connect", description: "setup account" },
     { command: "forward", description: "setup auto forward" },
     { command: "getuser", description: "Get User ID" },
@@ -18,9 +25,9 @@ bot.api.setMyCommands([
 ]);
 
 bot.catch((ctx) => {
-    console.error("Error Bot: " + ctx.message);
-    console.error("===========================");
-    // console.log(ctx);
+    console.error("Error Bot");
+    console.log(ctx);
+    
 })
 
-export { bot };
+export { bot, MyContext, MyConversation };
